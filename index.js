60337d2ebf7a0b1aa44ad5ac59bfd3508217e84e
JakeRArent <arentjake92@gmail.com>
2024-05-04 08:33:54 -0500

    Command Line Todo update for class on Saturday 5/4/24

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-const prompts = require('prompts');
+import prompts from 'prompts';
+
+var toDoList = [];
 
 const main = async () => {
-    const nextTodo = async() => {
+    const todoMenu = async() => {
         return await prompts({
-            type: 'text',
+            type: 'select',
             name: 'value',
-            message: 'What is your next todo item?',
+            message: 'What action to perform?',
+            choices: [
+                {title: "Show todo list", value: "S"},
+                {title: "Add to todo list", value: "A"},
+                {title: "Delete a todo list item", value: "D"},
+                {title: "Quit todo list", value: "Q"},
+            ],
     });
 };
-    var message = await nextTodo();
-    console.log(message);
+
+const addTodo = async() => {
+    return await prompts({
+        type: 'test',
+        name: 'value',
+        message: 'Whatis your new todo item?',
+    });
+    
+};
+//    var message = await nextTodo();
+ //   console.log(message);
+
+
+const showTodo = () => {
+    console.log("======================");
+        toDoList.forEach( (x) => console.log(x));
+    console.log("======================");
+};
+
+var option = {};
+do{
+    option = await todoMenu();
+    switch (option.value){
+        case 'S': 
+            showTodo();
+            break;
+        case 'A':
+            todoList.push(await addTodo());
+            break;
+
+    }
+} while(option.value != 'Q');
+
 };
 
 main().catch(console.error);
